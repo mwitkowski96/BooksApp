59bbe48f391996f990fceea407988fff6f7f4474
Marek Witkowski <marek.witkowski96@icloud.com>
2021-07-24 11:44:30 +0200

    Add books to fav.

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,23 @@
 {
   'use strict';
-
   const select = {
     templatesOf: {
       book: '#template-book',
     },
     listOf: {
       books: '.books-panel .books-list',
-    }
+    },
+
+    bookProperties: {
+      image: '.books-list .book__image',
+      id: 'data-id',
+    },
+  };
+
+  const classNames = {
+    book: {
+      favoriteImage: 'favorite',
+    },
   };
 
   const templates = {
@@ -16,7 +26,6 @@
 
 
   const render = function(){
-
     for(let book in dataSource.books){
       /* generate HTML based on template */
       const generatedHTML = templates.bookLink(dataSource.books[book]);
@@ -29,11 +38,35 @@
       /* add element to list */
       booksList.appendChild(book);
     }
-
   };
 
   render();
 
+  const favouriteBooks = [];
 
+  const initActions = function(){
 
+    const bookImages = document.querySelectorAll(select.bookProperties.image);
+    console.log(bookImages);
+
+    for(let bookImage of bookImages){
+      bookImage.addEventListener('dblclick', function(event){
+        event.preventDefault();
+
+        // const bookId = bookImage.getAttribute(select.bookProperties.id);
+        // console.log(bookId);
+
+        if(!bookImage.classList.contains(classNames.book.favoriteImage)){
+          bookImage.classList.add(classNames.book.favoriteImage);
+          favouriteBooks.push(bookImage);
+        } else if(bookImage.classList.contains(classNames.book.favoriteImage)){
+          bookImage.classList.remove(classNames.book.favoriteImage);
+          favouriteBooks.splice(favouriteBooks.indexOf(bookImage), 1);
+        }
+        console.log(favouriteBooks);
+      });
+    }
+  };
+
+  initActions();
 }
